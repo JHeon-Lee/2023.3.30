6628c0eff9b5a1a893b914933c1df654f5db39f3
notebook <ljh1642@gmail.com>
2023-04-13 20:11:24 +0900

    초기 커밋.

---
+++ 2023.3.30/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>      // 라이브러리 추가
+
+using namespace std;     // 전체적으로 사용되는 네임스페이스 영역 설정
+
+void main()
+{
+	// cout : 출력 명령어
+	// << : 명령 구분
+	// endl : 줄바꿈 문자
+	// ; : 명령이 끝났음을 알리는 문자 -- 주의
+	cout << "Hello World!" << endl <<  "안녕하세요" << endl;
+	// cout << "Hello World!" << endl;
+	// cout << "안녕하세요" << endl;
+	system("pause");
+}
